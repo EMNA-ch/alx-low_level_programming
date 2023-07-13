c60ac6f603b261366e0e717bca7e674d736dbe6e
Emna Ch <cheibi.emna90@gmail.com>
2023-07-13 12:15:30 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unisdt.h>
+/**
+ * main - a C program that prints a text to the standard error.
+ * Return: 1(Success)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n);
+return(1);
+}
