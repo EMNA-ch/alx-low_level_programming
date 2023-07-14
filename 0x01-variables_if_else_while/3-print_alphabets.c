5b9e2e3aa2c32947b6486f9b40c16593e74b4367
Emna Ch <cheibi.emna90@gmail.com>
2023-07-14 13:21:23 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a C program that prints the alphabet in uppercase.
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+char alphabet = 'A';
+while alphabet <= 'Z'
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
