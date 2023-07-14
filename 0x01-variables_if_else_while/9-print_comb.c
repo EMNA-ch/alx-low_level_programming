714f6be868f2f014d1171e83b8d48ba50ae9f18c
Emna Ch <cheibi.emna90@gmail.com>
2023-07-14 15:20:05 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints all possible combinations.
+ * Return: 0(Success)
+ */
+int main(void)
+{
+int n = 0;
+while (n <= 9)
+{
+putchar((n % 10) + '0');
+n++;
+if (n == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
