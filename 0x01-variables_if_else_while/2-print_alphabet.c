918a1e56e67b58128655647def63c5a63256b632
Emna Ch <cheibi.emna90@gmail.com>
2023-07-14 09:28:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase.
+ * Return: 0(Success)
+ */
+int main(void)
+{
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+return (0);
+}
