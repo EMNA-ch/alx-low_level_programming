6a4e4277db7cc587db8a596ba0f0dc3bdfe5b045
Emna Ch <cheibi.emna90@gmail.com>
2023-07-14 17:01:40 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints different combinations of three digits.
+ * Return: 0(Success)
+ */
+int main(void)
+{
+char num[4];
+int i, j, k;
+for (i = 0; i < 8; i++)
+{
+for (j = i + 1; j < 9; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+num[0] = i + '0';
+num[1] = j + '0';
+num[2] = k + '0';
+num[3] = '\0';
+putchar("%d\n", num);
+}
+}
+return (0);
+}
