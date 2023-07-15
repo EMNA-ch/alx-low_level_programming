144bd44f1f2745c332139829f81365605ee3ee52
Emna Ch <cheibi.emna90@gmail.com>
2023-07-14 17:28:00 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,9 +12,9 @@ for (j = i + 1; j < 9; j++)
 {
 for (k = j + 1; k < 10; k++)
 {
-putchar((i % 10) + '0');
-putchar((j % 10) + '0');
-putchar((k % 10) + '0');
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
 if (i == 7 && j == 8 && k == 9)
 putchar(',');
 putchar(' ');
