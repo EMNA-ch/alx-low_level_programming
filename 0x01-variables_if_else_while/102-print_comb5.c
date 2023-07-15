b2dc1f7704ba02a7f5aa106a647be2d281b571b2
Emna Ch <cheibi.emna90@gmail.com>
2023-07-14 18:09:02 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
+
 /**
- * main - a program that prints different combinations of three digits
- * Return: 0 (Success)
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, j, k;
-for (i = 48; i < 58; i++)
-{
-for (j = 49; j < 58; j++)
-{
-for (k = 50; k < 58; k++)
-{
-if (k > j && j > i)
-{
-putchar(i);
-putchar(j);
-putchar(k);
-if (i != 55 || j != 56)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int n, m, l;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
