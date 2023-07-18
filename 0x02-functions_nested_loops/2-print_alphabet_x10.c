7343d687611ebb7507c9e230be5fa74d83bff9e0
Emna Ch <cheibi.emna90@gmail.com>
2023-07-18 13:06:57 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * in lowercase using _putchar
+ */
+
+void print_alphabet_x10(void)
+{
+	int l. ch;
+
+	for (l = 0; l <= 9; l++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+	}
+	_putchar('\n');
+
+}
