991ca9dc67673fe44459cf6e1719864ea5ad1463
Emna Ch <cheibi.emna90@gmail.com>
2023-07-18 10:57:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,9 @@
 /**
  * _putchar - a program that writes a character c to stdout.
  * @c: the character to write
- * Return: 1(Success)
- * error: -1 is returned.
+ *
+ * Return: on success 1.
+ *         on erro -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
