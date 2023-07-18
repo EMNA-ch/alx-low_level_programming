c8d592a25a0d699b6c8dc7e425e9500b82a685a2
Emna Ch <cheibi.emna90@gmail.com>
2023-07-18 13:15:28 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,11 @@
 
 void print_alphabet_x10(void)
 {
-	int l, ch;
-
-	for (l = 0; l <= 9; l++)
-	{
-		for (ch = 'a'; ch <= 'z'; ch++)
-			_putchar(ch);
-		_putchar('\n');
-	}
+int l, ch;
+for (l = 0; l <= 9; l++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
 }
