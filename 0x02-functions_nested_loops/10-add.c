33f5ad37707f7927624be5943ca2d8a73fbeea56
Emna Ch <cheibi.emna90@gmail.com>
2023-07-18 17:35:55 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add : function that adds two integers and returns the result.
+ * @n: first integer to be added to m.
+ * @m: second integer to be added to n.
+ * Return: sum of n and m.
+ */
+int add(int n, int m)
+{
+	int sum;
+
+	sum = n + m;
+
+	return (sum);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,6 +13,8 @@ int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
+int add(int n, int m);
+void print_to_98(int n);
 
 
 
