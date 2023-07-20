e9566cf924cbe19b496fe407a08eea8041383089
Emna Ch <cheibi.emna90@gmail.com>
2023-07-20 16:36:02 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,6 @@
  * print_most_numbers - prints the numbers, from 0 to 9
  * followed by a new line
  * except 2 and 4.
- *
  * Return: 0(Success).
  */
 
@@ -18,4 +17,5 @@ void print_most_numbers(void)
 			_putchar(c);
 		_putchar('\n');
 	}
+	return (0);
 }
