8e4f9d559af8f437a7f228133ad9ee5963f9ad8a
EMNA-ch <cheibi.emna90@gmail.com>
2023-08-31 16:55:30 -0700

    task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * get_endianess - check endianness
+ * get_endianness - check endianness
  * Return: 0 if big endian, or 1 if little endian
  */
 int get_endianness(void)
 {
 	int num;
 
-	n = 1;
+	nm = 1;
 	if (*(char)&num == 1)
 		return (1);
 	else
